57125643796c75e087766d252270317960b01c30
Juan Pablo Brussini <jpbrusini1@gmail.com>
2021-01-26 09:58:18 -0300

    agregamos 7 htmls

---
+++ enviar2.php
@@ -0,0 +1,31 @@
+<?php
+
+$para = 'jpbrussini@hotmail.com';
+
+$asunto = 'Mensaje de mi sitio web';
+
+$nombree = $_POST['nombres'];
+$mails = $_POST['email'];
+$empresas = $_POST['mensajes'];
+
+$header = 'From: ' . $mails . " \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
+$header = "enviado desde jbvirtualconsulting";
+
+$mensajeCompleto = "MENSAJE: ". $empresas ." \n";
+$mensajeCompleto .="NOMBRE:". $nombree ."\n";
+$mensajeCompleto .= "E-MAIL: ". $mails ." \n";
+$mensajeCompleto .= "Enviado el " . date('d/m/Y', time());
+
+mail($para, $asunto, utf8_decode($mensajeCompleto), $header);
+
+header("Location:gracias.html");
+
+print_r($_POST);
+
+echo $nombree;
+
+?>
+
